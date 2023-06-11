3b02c08e7bc6d22425f191d84c6b5cb489abb6e0
tirta71 <tirta4132@gmail.com>
2023-06-11 17:58:22 +0700

    anjay

--- src/components/GetIdForm.js
+++ src/components/GetIdForm.js
@@ -1,28 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../css/Form/FormId.css";
 import FormDiamonds from "./Games/Mobile Legends/Form Diamonds/FormDiamonds";
+
 function GetIdForm() {
   const [userId, setUserId] = useState("");
   const [serverId, setServerId] = useState("");
   const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(
-        `https://v1.apigames.id/merchant/M230213DIHV6649WV/cek-username/mobilelegend?user_id=${userId}${serverId}&signature=7e3d1b5350485f4307d2f2086c49cb95`
-      );
+  useEffect(() => {
+    // Fungsi untuk memanggil API dan mendapatkan username
+    const fetchUsername = async () => {
+      try {
+        const response = await fetch(
+          `https://v1.apigames.id/merchant/M230213DIHV6649WV/cek-username/mobilelegend?user_id=${userId}${serverId}&signature=7e3d1b5350485f4307d2f2086c49cb95`
+        );
 
-      const data = await response.json();
-      setResponse(data);
-      setError(null);
-      console.log(data);
-    } catch (error) {
-      setResponse(null);
-      setError("Terjadi kesalahan saat mengambil data.");
+        const data = await response.json();
+        setResponse(data);
+        setError(null);
+        console.log(data);
+      } catch (error) {
+        setResponse(null);
+        setError("Terjadi kesalahan saat mengambil data.");
+      }
+    };
+
+    if (userId && serverId) {
+      fetchUsername(); // Panggil fungsi fetchUsername jika userId dan serverId tidak kosong
     }
-  };
+  }, [userId, serverId]);
 
   return (
     <div className="luar">
@@ -31,7 +38,7 @@ function GetIdForm() {
           <span className="satu">1</span>
           <h2>Masukan User ID</h2>
         </div>
-        <form onSubmit={handleSubmit}>
+        <form>
           <input
             type="text"
             id="user_id"
@@ -51,11 +58,23 @@ function GetIdForm() {
           />
         </form>
 
+        {response && response.data && response.data.username ? (
+          <div className="username-info" style={{ marginTop: "0.5rem" }}>
+            <p>Username : {response.data.username}</p>
+          </div>
+        ) : (
+          <div className="username-info">
+            <p>Username tidak ada</p>
+          </div>
+        )}
+
+        {error && <p className="error-message">{error}</p>}
+
         <span className="Footer-form">
           Untuk mengetahui User ID Anda, silakan klik menu profile dibagian kiri
           atas pada menu utama game. User ID akan terlihat dibagian bawah Nama
           Karakter Game Anda. Silakan masukkan User ID Anda untuk menyelesaikan
-          transaksi. Contoh : 12345678(1234).
+          transaksi. Contoh: 12345678(1234).
         </span>
       </div>
       <FormDiamonds />
