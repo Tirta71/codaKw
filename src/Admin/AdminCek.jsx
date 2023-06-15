039de0602a4cb12f9a9598fa6a86c1c279948408
tirta71 <tirta4132@gmail.com>
2023-06-15 11:43:00 +0700

    fixed bug

--- src/Admin/AdminCek.jsx
+++ src/Admin/AdminCek.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./AdminCek.css";
+import MyNavbar from "../components/navbar/MyNavbar";
 
 export default function AdminCek() {
   const [data, setData] = useState([]);
@@ -52,56 +53,59 @@ export default function AdminCek() {
   };
 
   return (
-    <div className="admin-cek-container">
-      <h2 className="admin-cek-title">Mobile Legend Data</h2>
-      <table className="admin-cek-table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Transaksi ID</th>
-            <th>Email</th>
-            <th>Username</th>
-            <th>ID Games</th>
-            <th>Jumlah Diamond</th>
-            <th>Status</th>
-            <th>Total Bayar</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.transactionId}</td>
-              <td>{item.email}</td>
-              <td>{item.username}</td>
-              <td>
-                {item.userId} ({item.serverId})
-              </td>
-              <td>{item.selectedDiamond.jumlah} Diamond</td>
-              <td>{item.status ? "Success" : "Pending"}</td>
-              <td>Rp. {item.totalBayar}</td>
-              <td>
-                {item.status ? (
-                  <button
-                    className="admin-cek-delete-btn"
-                    onClick={() => handleDeleteData(item.id)}
-                  >
-                    Delete
-                  </button>
-                ) : (
-                  <button
-                    className="admin-cek-update-btn"
-                    onClick={() => handleUpdateStatus(item.id)}
-                  >
-                    Update Status
-                  </button>
-                )}
-              </td>
+    <>
+      <MyNavbar />
+      <div className="admin-cek-container">
+        <h2 className="admin-cek-title">Mobile Legend Data</h2>
+        <table className="admin-cek-table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Transaksi ID</th>
+              <th>Email</th>
+              <th>Username</th>
+              <th>ID Games</th>
+              <th>Jumlah Diamond</th>
+              <th>Status</th>
+              <th>Total Bayar</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.transactionId}</td>
+                <td>{item.email}</td>
+                <td>{item.username}</td>
+                <td>
+                  {item.userId} ({item.serverId})
+                </td>
+                <td>{item.selectedDiamond.jumlah} Diamond</td>
+                <td>{item.status ? "Success" : "Pending"}</td>
+                <td>Rp. {item.totalBayar}</td>
+                <td>
+                  {item.status ? (
+                    <button
+                      className="admin-cek-delete-btn"
+                      onClick={() => handleDeleteData(item.id)}
+                    >
+                      Delete
+                    </button>
+                  ) : (
+                    <button
+                      className="admin-cek-update-btn"
+                      onClick={() => handleUpdateStatus(item.id)}
+                    >
+                      Update Status
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
